295692c520b3d65193807c4136ed1b61cf20b2fd
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2021-10-09 16:03:30 +0100

    feat(Fill colour with click & drag instead of hover)

--- app.js
+++ app.js
@@ -4,31 +4,47 @@
   const gridContainer = document.querySelector('#grid');
 
   let gridSize = 16;
+  let mouseDown = false;
+
+  function toggleMouseDown(event) {
+    mouseDown = event.type === 'mousedown';
+  }
+
+  function fillUnit(event) {
+    if (mouseDown) {
+      event.target.style.backgroundColor = '#9e9d9e';
+      event.target.removeEventListener('mousedown', fillUnit);
+      event.target.removeEventListener('mouseover', fillUnit);
+    }
+  }
+
+  function createGridUnit() {
+    const gridUnit = document.createElement('div');
+    gridUnit.classList.add('grid-unit');
+
+    const styling = gridUnitStyle();
+    Object.entries(styling).forEach(([key, value]) => {
+      gridUnit.style[key] = value;
+    });
+
+    applyGridUnitEventListeners(gridUnit);
+
+    return gridUnit;
+  }
+
+  function applyGridUnitEventListeners(unit) {
+    unit.addEventListener('dragend', toggleMouseDown);
+    unit.addEventListener('mouseup', toggleMouseDown);
+    unit.addEventListener('mousedown', toggleMouseDown);
+    unit.addEventListener('mousedown', fillUnit);
+    unit.addEventListener('mouseover', fillUnit);
+  }
 
   function gridUnitStyle() {
     return {
       width: `calc(100% / ${gridSize})`,
       height: `calc(100% / ${gridSize})`,
     }
-  }
-
-  function fillUnit(event) {
-    event.target.style.borderColor = '#9e9d9e';
-    event.target.style.backgroundColor = '#9e9d9e';
-  }
-
-  function createGridUnit() {
-    const gridUnit = document.createElement('div');
-    const styling = gridUnitStyle();
-
-    Object.entries(styling).forEach(([key, value]) => {
-      gridUnit.style[key] = value;
-    });
-
-    gridUnit.classList.add('grid-unit');
-    gridUnit.addEventListener('mouseover', fillUnit, { once: true });
-
-    return gridUnit;
   }
 
   function displayCleanGrid() {
